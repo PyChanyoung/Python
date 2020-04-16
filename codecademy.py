d0756c38517c9b08491abd729cb779da38eaabc4
ChanyoungPark88 <chanyoung.p.1988@gmail.com>
2020-04-16 17:57:51 -0600

    commit

--- codecademy.py
+++ codecademy.py
@@ -1,6 +1,36 @@
-def mult_two_add_three(number):
-  # number = 
-  print(number*2 + 3)
-  
-# Call mult_two_add_three() here:
-mult_two_add_three(1)
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+
+bomb_mass = 1
+
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32) * 5/9
+  return c_temp
+
+f100_in_celcius = f_to_c(100)
+print(f100_in_celcius)
+def c_to_f(c_temp):
+  f_temp = c_temp * (9/5) + 32
+  return f_temp
+c0_in_fahrenheit = c_to_f(0)
+print(c0_in_fahrenheit)
+
+def get_force(mass, acceleration):
+  force = mass * acceleration
+  return force
+
+train_force = get_force(train_mass, train_acceleration)
+print(train_force)
+
+print("The GE train supplies ", train_force, " Newtons of force.")
+
+def get_energy(mass,c = 3*10**8):
+  energy = mass * c
+  return energy
+
+print(get_energy(bomb_mass))
+
+def get_work(mass,acceleration,distance):
+  work = get_force(mass, acceleration) * distance
+  return work
---
+++ codecademy_1.py
@@ -0,0 +1,15 @@
+# Write your max_num function here:
+def max_num(num1, num2, num3):
+  largest = max(num1, num2, num3)
+  return largest
+
+# Uncomment these function calls to test your max_num function:
+
+print(max_num(-10, 0, 10))
+# should print 10
+print(max_num(-10, 5, -30))
+# should print 5
+print(max_num(-5, -10, -10))
+# should print -5
+print(max_num(2, 3, 3))
+# should print "It's a tie!"
